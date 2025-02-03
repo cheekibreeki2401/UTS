7aab5f179dd43692127a92b6a7fb2db254839020
Kyle Everett <keverett@xrx.com.au>
2025-02-03 16:54:26 +1000

    Add more windows support

--- src/curUser.c
+++ src/curUser.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
-#include <pwd.h>
+#ifdef __linux__
+	#include <pwd.h>
+#elif _WIN32
+	#include <Windows.h>
+#endif
 #include <unistd.h>
 #include <sys/types.h>
 #include <stdlib.h>
 
 const char *getUserName(){
-	uid_t uid = geteuid();
-	struct passwd *pw = getpwuid(uid);
-	if(pw){
-		return pw->pw_name;
-	} else {
-		return "ERR_NO_NAME";
-	}
+	#ifdef __linux__
+		uid_t uid = geteuid();
+		struct passwd *pw = getpwuid(uid);
+		if(pw){
+			return pw->pw_name;
+		} else {
+			return "ERR_NO_NAME";
+		}
+	#elif _WIN32
+		return getenv("USERNAME");
+	#endif
 }
